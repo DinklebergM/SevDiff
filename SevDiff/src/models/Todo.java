9dfbf1eed46680bfcc572f1250a73db0e9c3d273
Loewenherz12 <146241315+Loewenherz12@users.noreply.github.com>
2025-09-09 00:38:38 +0200

    Commit Todo

--- SevDiff/src/models/Todo.java
+++ SevDiff/src/models/Todo.java
@@ -1,5 +1,48 @@
 package models;
 
 public class Todo {
-int id;
+
+    private int id;
+    private String name;
+    private int date;
+    private boolean finished;
+
+    public Todo(int id, String name, int date, boolean finished){
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.finished = finished;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
 }
