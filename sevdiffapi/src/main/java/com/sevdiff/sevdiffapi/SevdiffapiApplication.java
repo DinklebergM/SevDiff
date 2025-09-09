2ee859455e21c319e907e338daddd580dc78cd9f
Dinkleberg <marcowolff17@gmail.com>
2025-09-10 00:35:00 +0200

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ sevdiffapi/src/main/java/com/sevdiff/sevdiffapi/SevdiffapiApplication.java
@@ -0,0 +1,13 @@
+package com.sevdiff.sevdiffapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevdiffapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevdiffapiApplication.class, args);
+	}
+
+}
---
+++ sevdiffapi/src/test/java/com/sevdiff/sevdiffapi/SevdiffapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevdiff.sevdiffapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevdiffapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
